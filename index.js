0152af5dc32ff80a37693de243485b6fa0fe4087
Frano Stanisic <fsta657@aucklanduni.ac.nz>
2018-08-30 22:39:07 +1200

    Make suggested change from raw PR

--- index.js
+++ index.js
@@ -320,8 +320,6 @@ bot.on('raw', async event => {
   const user = bot.users.get(data.user_id);
   const channel = bot.channels.get(data.channel_id) || await user.createDM();
 
-  if (channel.messages.has(data.message_id)) return;
-
   const message = await channel.fetchMessage(data.message_id);
   const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
   let reaction = message.reactions.get(emojiKey);
@@ -330,6 +328,8 @@ bot.on('raw', async event => {
     const emoji = new Discord.Emoji(bot.guilds.get(data.guild_id), data.emoji);
     reaction = new Discord.MessageReaction(message, emoji, 1, data.user_id === bot.user.id);
   }
+
+  if (reaction.users.has(user.id)) return;
 
   bot.emit(events[event.t], reaction, user);
 });
